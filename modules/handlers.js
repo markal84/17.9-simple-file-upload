dac68b68e5707c8c276ba84d40cf075f00b9f544
Marcin Kaleta <kalet1984@gmail.com>
2019-07-08 14:08:51 +0200

    Fix file upload problem

--- modules/handlers.js
+++ modules/handlers.js
@@ -4,10 +4,11 @@ var formidable = require('formidable');
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
+    form.uploadDir = "./"; // added to prevent cross-device link not permitted error
     form.parse(request, function(error, fields, files) {
-        //fs.renameSync(files.upload.path, "test.png"); // Disabled to prevent cross-device link not permitted error caused by formidable, because it is saving file to upload to a diffrent partition than project partition. But now upload from any other location than main folder is not working and /show is not working because it is not renaming file to test.png
+        fs.renameSync(files.upload.path, "test.png"); // Disabled to prevent cross-device link not permitted error caused by formidable, because it is saving file to upload to a diffrent partition than project partition. But now upload from any other location than main folder is not working and /show is not working because it is not renaming file to test.png
         response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
+        response.write("<h1>That is image you uploaded:</h1><br/>");
         response.write("<img src='/show' />");
         response.end();
     });
